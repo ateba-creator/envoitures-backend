076cfad4aea2961acbd67e597021872f4b7fba3f
ateba-creator <atebacreator@gmail.com>
2023-07-16 09:13:11 -0700

    error fixing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,4 @@
-sdkfsf
+<h1>Envoitures Rest API</h1>
 
-<img src="{{ asset('storage/vehicleImages/'.$vehicle->imageName) }}" alt="job image" title="job image">
+<!-- <img src="{{ asset('storage/vehicleImages/'.$vehicle->imageName) }}" alt="job image" title="job image"> -->
+
